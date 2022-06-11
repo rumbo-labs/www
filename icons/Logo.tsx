b7829bea422e5899593db4e4e347c51661420680
Luke Hartman <ljhartman10@gmail.com>
2022-06-10 17:51:45 -0700

    Initial commit

---
+++ icons/Logo.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="55"
+      height="55"
+      fill="none"
+      viewBox="0 0 55 55"
+    >
+      <path
+        fill="url(#paint0_linear_245_15)"
+        d="M55 27.5C55 42.688 42.688 55 27.5 55S0 42.688 0 27.5 12.312 0 27.5 0 55 12.312 55 27.5z"
+      ></path>
+      <path
+        fill="#fff"
+        d="M34.366 7.91c-7.305.072-13.593 5.1-15.324 12.255-1.731 7.154 1.527 14.646 7.94 18.258 6.414 3.612 14.38 2.442 19.413-2.853a19.576 19.576 0 01-25.573 9.854C10.984 41.22 6.087 29.96 9.72 19.896A19.576 19.576 0 0134.366 7.91z"
+      ></path>
+      <path
+        fill="#fff"
+        d="M34.724 35.486c.617-8.174 3.091-10.624 11.27-11.27-8.129-.676-10.604-3.138-11.235-11.312-.487 8.007-3.293 10.775-11.27 11.27 8.081.636 10.567 3.087 11.235 11.312z"
+      ></path>
+      <defs>
+        <linearGradient
+          id="paint0_linear_245_15"
+          x1="12.031"
+          x2="40.219"
+          y1="9.625"
+          y2="46.75"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F3B5FD"></stop>
+          <stop offset="1" stopColor="#53F5FF"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default Icon;
---
+++ icons/LogoSmall.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function LogoSmall() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="26"
+      height="26"
+      fill="none"
+      viewBox="0 0 26 26"
+    >
+      <path
+        fill="#000"
+        d="M26 13c0 7.18-5.82 13-13 13S0 20.18 0 13 5.82 0 13 0s13 5.82 13 13z"
+      ></path>
+      <path
+        fill="#fff"
+        d="M22.395 15.514a9.726 9.726 0 01-9.419-2.533 9.797 9.797 0 01-2.525-9.448 9.698 9.698 0 00-4.37 2.533c-3.807 3.82-3.807 10.012 0 13.83a9.729 9.729 0 0013.79 0 9.743 9.743 0 002.524-4.382z"
+      ></path>
+    </svg>
+  );
+}
+
+export default LogoSmall;
